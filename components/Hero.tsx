17fde573b299f08c155defdcac2f56573b72137a
Deekshitha-m <deekshitham12052001@gmail.com>
2024-07-04 17:09:27 +0530

    .

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -64,7 +64,7 @@ const Hero = () => {
            */}
           <TextGenerateEffect
             words={`Hi! I'm Deekshitha M, a Web Developer.`}
-            className="text-center text-3xl  md:text-5xl lg:text-[68px] text-white"
+            className="text-center text-[25px]  md:text-[45px] lg:text-[68px] text-white"
           />
 
           <TypeAnimation
